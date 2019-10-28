ad481ec2fd395b11934b5d530c4ef25ba6f6f90e
zhangs <zhangsgm@gmail.com>
2019-10-28 18:02:44 -0400

    add optionmenu and shared preferences

--- app/src/main/java/com/example/alicepuppies/MainActivity.java
+++ app/src/main/java/com/example/alicepuppies/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.alicepuppies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -19,6 +23,9 @@ import android.widget.Toast;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Date;
 
 //https://android.jlelse.eu/the-complete-android-splash-screen-guide-c7db82bce565
 // use launch theme
@@ -68,12 +75,12 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
 
         loadfromtextfile();
-
         Toast toast = Toast.makeText(getApplicationContext(),
                 everything,
                 Toast.LENGTH_LONG);
-
         toast.show();
+
+        setsp();
 
 
         // storing string resources into Array
@@ -100,5 +107,84 @@ public class MainActivity extends ListActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.puppy_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.setsp:
+                setsp();
+                return true;
+            case R.id.getsp:
+                getsp();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    void getsp()
+    {
+        SharedPreferences prefs = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
+
+        Date myDate = new Date(prefs.getLong("time", 0));
+        Toast toast = Toast.makeText(getApplicationContext(),
+                myDate.toString(),
+                Toast.LENGTH_LONG);
+        toast.show();
+
+    }
+    void setsp()
+    {
+        SharedPreferences prefs = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
+        SharedPreferences.Editor editor = prefs.edit();
+
+        //getting the current time in milliseconds, and creating a Date object from it:
+        Date date = new Date(System.currentTimeMillis()); //or simply new Date();
+
+        //converting it back to a milliseconds representation:
+        long millis = date.getTime();
+        prefs.edit().putLong("time", date.getTime()).apply();
+
+
+        editor.commit(); // commit changes
+
+    }
+
+    void readremotetextfile()
+    {
+        String whole="Text is: ";
+
+        try {
+            // Create a URL for the desired page
+            URL url = new URL("http://www.gutenberg.org/cache/epub/14606/pg14606.txt");
+            // Read all the text returned by the server
+            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+            String str;
+
+            while ((str = in.readLine()) != null) {
+                // str is one line of text; readLine() strips the newline character(s)
+                whole+=str+"\n";
+            }
+            in.close();
+        } catch (MalformedURLException e) {
+            Log.d("readtext","malformedurl");
+        } catch (IOException e) {
+        }
+
+        Toast toast = Toast.makeText(getApplicationContext(),
+                whole,
+                Toast.LENGTH_LONG);
+        toast.show();
+
+
+
+    }
 
 }
